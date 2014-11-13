a2608e3f563c676c892068b876c310c6ba991728
davidebbo <david.ebbo@microsoft.com>
2014-11-12 18:21:07 -0800

    Initial commit

---
+++ WebApplicationWithModule/DummyModule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Web;
+
+namespace WebApplicationWithModule
+{
+    public class DummyModule : IHttpModule
+    {
+        public void Init(HttpApplication app)
+        {
+            app.BeginRequest += OnBeginRequest;
+            app.EndRequest += OnEndRequest;
+        }
+
+        private static void OnBeginRequest(object sender, EventArgs e)
+        {
+        }
+
+        private static void OnEndRequest(object sender, EventArgs e)
+        {
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
